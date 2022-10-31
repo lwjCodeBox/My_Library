45e969256bbd0ad09919e69d0c8d7d9328ce68db
lwj789_CodeBox <lwj798@naver.com>
2022-10-31 09:51:48 +0900

    [Add]   1.  UTF8 <-> 유니코드 문자열 변환 기능 추가
    2022.10.31 - 09:51

--- String/WJ_String.h
+++ String/WJ_String.h
@@ -20,6 +20,45 @@ public:
 	virtual ~WJ_String();
 
 	void AsciiToUnicode(char *ap_string);
+
+	// UTF8형식의 문자열을 유니코드 형태로 변경하는 함수. 이 함수의 반환 값이
+	// 유니코드로 변경된 문자열의 주소이다. 이 주소는 동적할당된 메모리라서 
+	// 받은쪽에서 반드시 delete[] 연산자로 삭제해야 한다.
+	wchar_t *UTF8_To_Unicode(const char *ap_src_string)
+	{
+		// \0 까지 포함한 크기가 반환됨
+		int temp_length = MultiByteToWideChar(CP_UTF8, 0, ap_src_string, -1, NULL, 0);
+		// 문자열 변환을 위해서 메모리를 할당한다.
+		wchar_t *p_unicode_str = new wchar_t[temp_length];
+		// 메모리 할당에 성공했다면 UTF8 형식의 문자열을 유니코드 문자열로 변환한다.
+		if (p_unicode_str != NULL)
+			MultiByteToWideChar(CP_UTF8, 0, ap_src_string, -1, p_unicode_str, temp_length);
+		return p_unicode_str;
+	}
+
+	// UTF8 문자열을 유니코드 문자열로 변환하는 함수
+	inline int UTF8_To_Unicode(wchar_t *ap_dest_string, const char *ap_src_string)
+	{
+		// \0 까지 포함한 크기가 반환됨
+		int temp_length = MultiByteToWideChar(CP_UTF8, 0, ap_src_string, -1, NULL, 0);
+		if (ap_dest_string != NULL) {
+			MultiByteToWideChar(CP_UTF8, 0, ap_src_string, -1, ap_dest_string, temp_length);
+		}
+		return temp_length - 1;
+	}
+
+	// 유니코드 문자열을 UTF8 문자열로 변환하는 함수
+	inline int Unicode_To_UTF8(char *ap_dest_string, const wchar_t *ap_src_string)
+	{
+		// 유니코드 형식의 문자열을 UTF8 형식으로 변경했을때 필요한 메모리 공간의 크기를 얻는다.
+		int temp_length = WideCharToMultiByte(CP_UTF8, 0, ap_src_string, -1, NULL, 0, NULL, NULL);
+		// 유니코드를 UTF8코드로 변환한다.
+		if (ap_dest_string != NULL) {
+			WideCharToMultiByte(CP_UTF8, 0, ap_src_string, -1, ap_dest_string, temp_length, NULL, NULL);
+		}
+		return temp_length;
+	}
+	
 	void InitialObject(const wchar_t *ap_string); // 객체 초기화 함수
 	/*inline*/ int GetLength();				// 문자열의 길이를 알려주는 함수
 	/*inline*/ const wchar_t *GetString();	// 문자열의 내용을 알려주는 함수
